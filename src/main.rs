ec836b4eaf3f8c2d1ac827a71bcffd12a3e3e831
Sebastian C. Sabry <sebastiancsabry@system76-pc.localdomain>
2023-12-17 17:58:01 -0800

    Commiting on master to checkout

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
-use quicksilver::{geom::Vector, run, Graphics, Input, Result, Settings, Window};
-struct Game;
+use quicksilver::{
+    geom::Vector,
+    graphics::{Color, Image, VectorFont},
+    run, Graphics, Input, Result, Settings, Window,
+};
+struct Game {
+    title: Image,
+    font_info: Image,
+}
 
-impl State for Game {
+impl Game {
     // Load the assets, initialise the Game
-    fn new() -> Result<Self> {
-        Ok(Self)
+    async fn new(window: Window, mut gfx: Graphics, mut input: Input) -> Result<Self> {
+        gfx.clear(Color::BLACK);
+        let ttf = VectorFont::load("mononoki-Regular.ttf").await?;
+        let title = ttf.to_renderer(&gfx, 72.0);
+        Ok(Self { title, font_info })
     }
 
     //Process keybourd, mouse input to update the game state
@@ -20,9 +30,15 @@ impl State for Game {
 
 fn main() {
     let settings = Settings {
+        title: "The rouge game",
         ..Default::default()
     };
     run(settings, app)
 }
 
-async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {}
+async fn app(window: Window, mut gfx: Graphics, mut input: Input) -> Result<()> {
+    gfx.present(&window);
+    loop {
+        while let Some(_) = input.next_event().await {}
+    }
+}
